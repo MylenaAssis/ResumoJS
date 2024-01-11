2b46322c7782ec36effc40771e49e59c6e3e319f
Mylena Assis <lena.assis.dev@gmail.com>
2024-01-11 09:52:17 -0300

    Added - Sintaxe basica

---
+++ index.js
@@ -0,0 +1,16 @@
+//Estudo da sintaxe JS
+//O JS é case sensitive.
+
+//exibir aviso de erro
+console.error("Mensagem de erro.");
+//exibir aviso
+console.warn("Mensagem de aviso.");
+//exibir informação
+console.info("Mensagem de informação");
+//popup - usado para direcionar mensagem ao usuario
+alert("Mensagem do popup");
+//Mensagem deixadas com console.log() serão lidas pelo dev tools
+
+//criando variável
+//o JS aceita uso de _ e $ na declaração das variáveis
+let nome = "Gian"
