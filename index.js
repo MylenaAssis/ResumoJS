67b63c4e0bd16df40a6cc9aa2c04b2fefdb924bf
Mylena Assis <lena.assis.dev@gmail.com>
2024-01-11 14:44:18 -0300

    Added - Comandos uteis

--- index.js
+++ index.js
@@ -34,3 +34,35 @@ nome.slice(0,3)
 //buscar caracteres na string, ele retorna a posição
 nome.search('a')
 
+//declarar tipo lógico
+let feriado = true
+
+//Em JS basicamente so se usa number para trabalhar com numeros
+let nota = 10.5 //no type of imprime number, mas também é conhecido como float
+//Para verificar se um numero é inteiro ou não, utilizar:
+Number.isInteger(nota); //vai retornar true or false
+
+//operações
+10**2 //potenciação
+10%3 //resto da divisão
+
+//converter numero que foi passado como string para number
+parseFloat(variavel)
+
+//converter decimal para inteiro
+parseInt(variavel)
+
+//se no desenvolvimento for necessário converter uma string em numero, ela pode apresentar erro por não ter sido passado um numero. Nesse caso fazer a verificação:
+parseFloat(variavelTexto) //vai aparecer o erro NaN = not a number
+Number.isNaN(parseFloat(variavelTexto)) // essa verificação vai retornar true or false
+
+//array = conjunto de dados
+let TecWeb = ["CSS", "HTML", "JAVASCRIPT"];
+//o array trabalha com índice, inicando do 0
+TecWeb.length //mostra o tamanho
+TecWeb[3] //mostra qual caractere esta nessa posição
+TecWeb.push["Adicional"] //adiciona mais um item ao array, retorna a quantidade atualizada de itens do array
+
+//Os array nao sao um tipo nativo do javascript. No typeof ele retorna object que é o tipo mais primitivo do JS. O array do JS não precisa ser homogeneo, comporta diversos tipos.
+
+
