a65660fe1312098fd54ec6f53ca65405cfeae89d
Mylena Assis <lena.assis.dev@gmail.com>
2024-01-11 10:00:49 -0300

    Added: Tipos de dados

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ alert("Mensagem do popup");
 //criando variável
 //o JS aceita uso de _ e $ na declaração das variáveis
 let nome = "Gian"
+
+//constante - não pode ser reatribuida
+const a = 3;
+
+//conferindo o tipo da variavel
+typeof nome;
+
+//opções para concatenação (as duas incluem espaço entre as variaveis)
+nome + ' ' + a;
+`$[nome] $[a]`
+
+//contar caracteres da string
+nome.length
+
+//selecionando pedaço da string, parametro primeiro caractere e ultimo desejado
+nome.slice(0,3)
+
+//buscar caracteres na string, ele retorna a posição
+nome.search('a')
+
